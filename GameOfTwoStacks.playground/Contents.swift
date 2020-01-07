61ac27f875db55ca47990a72691168d1a7545bc9
Stephen Brown <stephen.brown@reachplc.com>
2020-01-07 17:24:59 +0000

    Go backwards

--- GameOfTwoStacks.playground/Contents.swift
+++ GameOfTwoStacks.playground/Contents.swift
@@ -8,7 +8,6 @@ func calculateSums(x: Int, array: [Int]) -> [Int] {
     })
 }
 
-// Iterate incrementally
 func twoStacks(x: Int, a: [Int], b: [Int]) -> Int {
     let aSums = calculateSums(x: x, array: a)
     let bSums = calculateSums(x: x, array: b)
@@ -20,6 +19,11 @@ func twoStacks(x: Int, a: [Int], b: [Int]) -> Int {
         }
     }
     
+    return twoStacksBackwards(x: x, aSums: aSums, bSums: bSums)
+}
+
+// less efficient approach
+func twoStacksForwards(x: Int, aSums: [Int], bSums: [Int]) -> Int {
     var maxTurns = 0
     for aIndex in 0..<aSums.count {
         for bIndex in 0..<bSums.count {
@@ -40,18 +44,8 @@ func twoStacks(x: Int, a: [Int], b: [Int]) -> Int {
     return maxTurns
 }
 
-// Iterate decrementally
-func twoStacksBackwards(x: Int, a: [Int], b: [Int]) -> Int {
-    let aSums = calculateSums(x: x, array: a)
-    let bSums = calculateSums(x: x, array: b)
-    
-    // does max work?
-    if (aSums.count - 1) == a.count, (bSums.count - 1) == b.count {
-        if (aSums[aSums.count - 1] + bSums[bSums.count - 1]) <= x {
-            return aSums.count + bSums.count - 2
-        }
-    }
-    
+// better approach - go from the back of the array
+func twoStacksBackwards(x: Int, aSums: [Int], bSums: [Int]) -> Int {
     var maxTurns = 0
     for aCounter in 1...aSums.count {
         for bCounter in 1...bSums.count {
@@ -75,16 +69,13 @@ let a = [4, 2, 4, 6, 1]
 let b = [2, 1, 8, 5]
 
 print(twoStacks(x: 10, a: a, b: b))
-print(twoStacksBackwards(x: 10, a: a, b: b))
 
 let i = [7, 15, 12, 0, 5, 18, 17, 2, 10, 15, 4, 2, 9, 15, 13, 12, 16]
 let j = [12, 16, 6, 8, 16, 15, 18, 3, 11, 0, 17, 7, 6, 11, 14, 13, 15, 6, 18, 6, 16, 12, 16, 11, 16, 11]
 
 print(twoStacks(x: 62, a: i, b: j))
-print(twoStacksBackwards(x: 62, a: i, b: j))
 
 let x = [15, 12, 14, 4, 5, 6, 10, 13, 7, 8, 18, 11, 6, 10, 1, 11, 16, 0, 1, 5, 16, 16, 6, 9, 9, 15, 16, 12, 12, 9, 6, 7, 2, 0, 11, 7, 18, 13, 12, 5, 14, 10, 9, 0, 12, 10]
 let y = [11, 9, 11, 5, 14, 19, 13, 12, 8, 2, 19, 17, 6, 11, 6, 12, 10, 8, 5, 13, 8, 3, 7, 0, 1, 13, 3, 10, 13, 15, 0, 16, 16, 3, 13, 3, 3, 6, 15, 3, 8, 7]
 
 print(twoStacks(x: 75, a: x, b: y))
-print(twoStacksBackwards(x: 75, a: x, b: y))
